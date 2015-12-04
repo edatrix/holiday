d5722d02a52cf20b840e81e1221c9f8bfcdcc49a
edatrix <lespath@gmail.com>
2015-12-03 23:11:26 -0700

    sets up helper to make view dynamic, just needs images

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,7 @@
 class StaticController < ApplicationController
 
   def index
-    @q1 = ["January", "February", "March"]
-    @q2 = ["April", "May", "June"]
-    @q3 = ["July", "August", "September"]
-    @q4 = ["October", "November", "December"]
+    @months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,66 @@
 module ApplicationHelper
+
+  MONTH_IMAGES = [
+    {
+      :month => 1,
+      :name => "our family",
+      :image => "family.JPG"
+    },
+    {
+      :month => 2,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 3,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 4,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 5,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 6,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 7,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 8,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 9,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 10,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 11,
+      :name => "",
+      :image => ""
+    },
+    {
+      :month => 12,
+      :name => "",
+      :image => ""
+    }
+  ]
+
 end
