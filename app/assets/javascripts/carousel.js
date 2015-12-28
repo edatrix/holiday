24bde33138853f4a2714da7b8a4305b3735485d0
edatrix <lespath@gmail.com>
2015-12-27 21:47:07 -0700

    clean up files, remove indicators, chevrons work

--- app/assets/javascripts/carousel.js
+++
@@ -1,9 +0,0 @@
-  // dynamically make carousel idicators on NPS homepage
-  var myCarousel = $(".carousel");
-  myCarousel.append("<ol class='carousel-indicators'></ol>");
-  var indicators = $(".carousel-indicators");
-  myCarousel.find(".carousel-inner").children(".item").each(function(index) {
-    (index === 0) ?
-    indicators.append("<li data-target='#feature_carousel' data-slide-to='"+index+"' class='active'></li>") :
-    indicators.append("<li data-target='#feature_carousel' data-slide-to='"+index+"'></li>");
-  });
--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -102,15 +102,3 @@ Number.prototype.clamp = function(min, max) {
   window.Sprite.EMPTY = LoaderProxy();
   window.Sprite.load = Sprite.load;
 }());
-
-function carouselIndicators() {
-  // dynamically make carousel idicators on NPS homepage
-  var myCarousel = $(".carousel");
-  myCarousel.append("<ol class='carousel-indicators'></ol>");
-  var indicators = $(".carousel-indicators");
-  myCarousel.find(".carousel-inner").children(".item").each(function(index) {
-    (index === 0) ?
-    indicators.append("<li data-target='#myCarousel' data-slide-to='"+index+"' class='active'></li>") :
-    indicators.append("<li data-target='#myCarousel' data-slide-to='"+index+"'></li>");
-  });
-}
