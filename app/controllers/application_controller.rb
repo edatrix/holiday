35ed77360b892c16f6a4be4ab478046c1e06f909
edatrix <lespath@gmail.com>
2016-03-20 11:01:59 -0600

    setup permanent redirect if they hit the old domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :redirect_if_old
+
+  protected
+
+  APP_DOMAIN = 'http://spluhring-holiday-2015.herokuapp.com/'
+
+  def redirect_if_old
+    if request.host == 'http://spluhring-2015.com/'
+      redirect_to "http://#{APP_DOMAIN}#{request.request_uri}", :status => 301
+    end
+  end
+
 end
