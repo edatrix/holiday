93f6024112153ded6b9bc7c940aef94133249f2d
edatrix <lespath@gmail.com>
2016-12-20 21:50:05 -0700

    styling, 2016

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( family.JPG )
+Rails.application.config.assets.precompile +=
+  %w( family.JPG
+      two016.css )
